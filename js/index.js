05cb9181aa0b4bc5626dbf729d68c49c5c472605
Thanh Nguyen Kim <v3180180@transcosmos-vn.local>
2019-07-10 16:45:44 +0700

    udpate 10/7/2019

--- js/index.js
+++ js/index.js
@@ -31,3 +31,42 @@ $('.skills__content').slick({
     }
   ]
 });
+$('.skills__content-02').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 6,
+  slidesToScroll: 6,
+  prevArrow: '<div class="btn-slider btn-left"><i class = "fas fa-chevron-left"></i></div>',
+  nextArrow: '<div class="btn-slider btn-right"><i class="fas fa-chevron-right"></i></div>',
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+  e.target
+  e.relatedTarget
+  $('.skills__content').slick('setPosition');
+});
