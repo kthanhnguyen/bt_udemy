5dcc4f2745f2e592e1e87c54d742cd382d0e6cfa
Thanh Nguyen Kim <v3180180@transcosmos-vn.local>
2019-07-19 16:52:36 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,117 +1,66 @@
 $(document).ready(function() {
-  $('.skills__content').slick({
-    dots: false,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    prevArrow: '<div class="btn-slider btn-left"><i class = "fas fa-chevron-left"></i></div>',
-    nextArrow: '<div class="btn-slider btn-right"><i class="fas fa-chevron-right"></i></div>',
-    responsive: [{
-        breakpoint: 1025,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
-  $('.skills__content-02').slick({
-    dots: false,
-    infinite: false,
-    speed: 300,
-    // slidesToShow: 6,
-    // slidesToScroll: 6,
-    prevArrow: '<div class="btn-slider btn-left"><i class = "fas fa-chevron-left"></i></div>',
-    nextArrow: '<div class="btn-slider btn-right"><i class="fas fa-chevron-right"></i></div>',
-    mobileFirst: true,
-    responsive: [{
-        breakpoint: 1400,
-        settings: {
-          slidesToShow: 6,
-          slidesToScroll: 6
-        }
-      }, {
-        breakpoint: 1025,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4
-        }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
 
+  $('.skills__content').owlCarousel({
+    loop: false,
+    items: 4,
+    nav: true,
+    dots: false,
+    margin: 15,
+    navText: ["<div class='btn-slider btn-left'><i class = 'fas fa-chevron-left'></i></div>", "<div class='btn-slider btn-right'><i class='fas fa-chevron-right'></i></div>"],
+    responsiveclass: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      }
+    }
+  })
   $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
-    e.target
-    e.relatedTarget
-    $('.skills__content').slick('setPosition');
+    e.target // newly activated tab
+    e.relatedTarget // previous active tab
+    $(".owl-carousel").trigger('refresh.owl.carousel');
   });
 
-  $('.testimonials__content').slick({
-      dots: false,
-      infinite: false,
-      speed: 300,
-      // slidesToShow: 3,
-      // slidesToScroll: 3,
-      rows: 0,
-      prevArrow: '<div class="btn-slider btn-left"><i class ="fas fa-chevron-left"></i></div>',
-      nextArrow: '<div class="btn-slider btn-right"><i class="fas fa-chevron-right"></i></div>',
-      mobileFirst: true,
-      responsive: [{
-          breakpoint: 1400,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            dots: false,
-          }
-        }, {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            dots: false,
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: true,
-            arrows: false
-          }
-        }
-      ]
-    })
-    .on('setPosition', function(event, slick) {
-      slick.$slides.css('height', slick.$slideTrack.height() + 'px');
-    });
+  $('.skills__content-02').owlCarousel({
+    loop: true,
+    margin: 15,
+    nav: true,
+    dots: false,
+    navText: ["<div class='btn-slider btn-left'><i class = 'fas fa-chevron-left'></i></div>", "<div class='btn-slider btn-right'><i class='fas fa-chevron-right'></i></div>"],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 6
+      }
+    }
+  })
+  $('.testimonials__content').owlCarousel({
+    loop: true,
+    items: 4,
+    nav: true,
+    dots: false,
+    navText: ["<div class='btn-slider btn-left'><i class = 'fas fa-chevron-left'></i></div>", "<div class='btn-slider btn-right'><i class='fas fa-chevron-right'></i></div>"],
+    autoHeight: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 3
+      }
+    }
+  })
 });
